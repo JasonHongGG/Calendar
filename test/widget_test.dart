f17d3f9df6c42f128bb91f6cc89593e9c8219cf5
JasonHongGG <p76134040@gs.ncku.edu.tw>
2026-01-31 20:31:56 +0800

    chore: update .gitignore and remove tracked generated files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calendar_app/main.dart';
+import 'package:calendar/main.dart';
 
 void main() {
   testWidgets('Calendar app smoke test', (WidgetTester tester) async {
